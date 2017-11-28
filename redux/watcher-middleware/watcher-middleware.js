f51d848cde64ba68cef95ac3a2f49364ead2f8ed
PanyushkinD <PanyushkinD@quantumart.ru>
2017-11-28 09:21:49 +0300

    Optimize middleware

--- redux/watcher-middleware/watcher-middleware.js
+++ redux/watcher-middleware/watcher-middleware.js
@@ -1,13 +1,17 @@
-export default function (watchers) {
+export default function(watchers) {
   return ({ dispatch, getState }) => next => action => {
     const result = next(action);
     const state = getState();
-    watchers.forEach(watcher => {
-      const nextAction = watcher(state, action);
+
+    const length = watchers.length;
+    for (let i = 0; i < length; i++) {
+      const nextAction = watchers[i](state, action);
+
       if (nextAction) {
         dispatch(nextAction);
       }
-    });
+    }
+
     return result;
   };
 }
