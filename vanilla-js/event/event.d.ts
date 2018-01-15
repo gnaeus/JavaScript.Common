74892a0c48ddfd062ddb40d4cd26ab1b5dafbbd5
PanyushkinD <PanyushkinD@quantumart.ru>
2018-01-15 10:05:09 +0300

    Improve type definitions

--- vanilla-js/event/event.d.ts
+++ vanilla-js/event/event.d.ts
@@ -2,18 +2,30 @@
  * Factory that creates event function.
  * Which delegates it's execution to list of handlers.
  */
-export declare function event<T = (...args: any[]) => void>(): Delegate<T>;
+export declare function event<TFunction = (...args: any[]) => void>(): Delegate<TFunction>;
+export declare function event<TData>(
+  dataType: new () => TData,
+): Delegate<(data: TData) => void>;
+export declare function event<TSender, TData>(
+  senderType: new () => TSender,
+  dataType: new () => TData,
+): Delegate<(sender: TSender, data: TData) => void>;
+export declare function event<TSender, TData, TExtra>(
+  senderType: new () => TSender,
+  dataType: new () => TData,
+  extraType: new () => TExtra,
+): Delegate<(sender: TSender, data: TData, extra: TExtra) => void>;
 
 /** Represents a general typed event */
-export type Delegate<T> = T & {
+export type Delegate<TFunction> = TFunction & {
   /**
    * Add the specified handler function to the list of event handlers.
    */
-  attach(handler: T): void;
+  attach(handler: TFunction): void;
   /**
    * Removes handler, previously registered with `.attach()`, from the event handlers list.
    */
-  detach(handler: T): void;
+  detach(handler: TFunction): void;
 }
 
 /** Represents the typed event when the event provides sender and data. */
