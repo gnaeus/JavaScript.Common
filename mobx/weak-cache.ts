674df3d8d77adcf76fbf2b28cda8bdebcbfc343a
PanyushkinD <PanyushkinD@quantumart.ru>
2018-11-09 12:34:45 +0300

    Improve ComputedCache

--- mobx/weak-cache.ts
+++ mobx/weak-cache.ts
@@ -26,6 +26,11 @@ export class WeakCache<TKey extends object = object, TValue = any> {
 export class ComputedCache<TKey extends object = object, TValue = any> {
   private _weakMap = new WeakMap<object, IComputedValue<any>>();
 
+  public get<V extends TValue>(key: TKey): V | undefined {
+    const computedValue = this._weakMap.get(key);
+    return computedValue && computedValue.get();
+  }
+
   public getOrAdd<K extends TKey, V extends TValue>(key: K, getValue: () => V): V;
   public getOrAdd<K extends TKey, V extends TValue>(
     key: K,
